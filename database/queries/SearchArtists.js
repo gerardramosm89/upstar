641fa995615dd8b2b5f544036beceed38c4159cb
Cudablogv2 <gerard.ramosm89@gmail.com>
2017-04-16 11:51:33 -0700

    changes

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,5 +9,46 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
-  
+  var sortOrder = {};
+  sortOrder[sortProperty] = 1;
+  const query = Artist.find(buildQuery(criteria)).sort(sortOrder).skip(offset).limit(limit)
+    .then(users => {
+      return users;
+    });
+
+  return Promise.all([query, Artist.count()])
+    .then((results) => {
+      return {
+        all: results[0],
+        count: results[1],
+        offset: offset,
+        limit: limit
+      };
+    });
+
+};
+
+const buildQuery = (criteria) => {
+  const query = {
+
+  };
+  if (criteria.name) {
+    
+  }
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    };
+  }
+
+  return query;
 };
